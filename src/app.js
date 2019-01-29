6a00e02a89d079874d033544113a0a9b78e5318b
Fiyin <kolawolemmanuel@hotmail.com>
2019-01-29 14:14:47 +0100

    Add catch all route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import logger from 'morgan';
 import bodyparser from 'body-parser';
+import expressValidator from 'express-validator';
+
 import partyRouter from './routes/party';
 import officeRouter from './routes/office';
-
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(logger('dev'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
+app.use(expressValidator());
 
 app.get('/', (req, res) => {
   return res.status(200).json({
@@ -21,6 +23,12 @@ app.get('/', (req, res) => {
 app.use('/api/v1/parties', partyRouter);
 app.use('/api/v1/offices', officeRouter);
 
+app.get('*', (req, res) => {
+  res.status(404).json({
+    status: 404,
+    error: 'Not found! Check that you have the correct url',
+  });
+});
 
 app.listen(port, () => console.log(`app running on port ${port}`));
 
